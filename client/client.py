7859558cd75eeb348d9e9380442225d7e3ab1b15
Radu Chelaru <raduchelaru1@gmail.com>
2022-12-11 15:33:22 +0200

    Reformatting

--- client/client.py
+++ client/client.py
@@ -4,8 +4,10 @@ import threading
 import tkinter as tk
 import emoji
 import io
+import tkinter as tk
+from tkinter import scrolledtext, messagebox, filedialog
 from base64 import b64encode, b64decode
-from tkinter import scrolledtext, messagebox ,filedialog
+from tkinter import scrolledtext, messagebox, filedialog
 from PIL import Image, ImageTk
 
 HOST = '127.0.0.1'
@@ -21,7 +23,8 @@ SMALL_FONT = ("Helvetica", 13)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def create_message_dic(sender, receiver,message_type, content):
+
+def create_message_dic(sender, receiver, message_type, content):
     return {
         "sender": sender,
         "receiver": receiver,
@@ -34,14 +37,16 @@ def add_message(message):
     message_box.config(state=tk.NORMAL)
     message_box.insert(tk.END, emoji.emojize(message) + '\n')
     message_box.config(state=tk.DISABLED)
-    
+
+
 def connect():
     try:
         client.connect((HOST, PORT))
         print("Successfully connected to server")
         add_message("[SERVER] Successfully connected to the server")
     except:
-        messagebox.showerror("Unable to connect to server", f"Unable to connect to server {HOST} {PORT}")
+        messagebox.showerror("Unable to connect to server",
+                             f"Unable to connect to server {HOST} {PORT}")
 
     global username
     username = username_textbox.get()
@@ -51,47 +56,79 @@ def connect():
     else:
         messagebox.showerror("Invalid username", "Username cannot be empty")
 
-    threading.Thread(target=listen_for_messages_from_server, args=(client, )).start()
+    threading.Thread(target=listen_for_messages_from_server,
+                     args=(client, )).start()
 
     username_textbox.config(state=tk.DISABLED)
     username_button.config(state=tk.DISABLED)
 
+
 def send_text():
     message = message_textbox.get()
-    
+
     if message != '':
         dic = create_message_dic(username, "", "message", message)
-       
+
         client.sendall(str(dic).encode())
         message_textbox.delete(0, len(message))
     else:
         messagebox.showerror("Empty message", "Message cannot be empty")
-        
+
+
 def send_image():
-    path=filedialog.askopenfilename(filetypes=[("Image File",'.jpg')])
-    
+    path = filedialog.askopenfilename(filetypes=[("Image File", '.jpg')])
+
     image_handle = open(path, 'rb')
     raw_image_data = image_handle.read()
-    
+
     encoded_data = b64encode(raw_image_data)
-    # decoded_data = b64decode(encoded_data)
-    
+
     dic = create_message_dic(username, "", "image", encoded_data)
     client.sendall(str(dic).encode())
-    
-    
-    
     im = Image.open(path)
-    
+
+
+def handle_img_received(b64image):
+    raw_data = b64decode(b64image)
+    stream = io.BytesIO(raw_data)
+    img = Image.open(stream)
+
+    global recv_img
+    recv_img = ImageTk.PhotoImage(img)
+
+    message_box.config(state=tk.NORMAL)
+    message_box.image_create(tk.END, image=recv_img)
+    message_box.config(state=tk.DISABLED)
+
+
+def listen_for_messages_from_server(client):
+
+    while 1:
+
+        message = client.recv(16384).decode()
+        if message != '':
+            dic_received = eval(message)
+
+            username = dic_received["sender"]
+            content = dic_received["content"]
+
+            if dic_received["type"] == "image":
+                handle_img_received(content)
+            else:
+                add_message(f"[{username}] {content}")
+
+        else:
+            messagebox.showerror(
+                "Error", "Message recevied from client is empty")
+
 
 def onMessageReturnPress(*arg):
     send_text()
 
+
 def onIdReturnPress(*arg):
     connect()
-    
-    
-    
+
 
 root = tk.Tk()
 root.geometry("600x600")
@@ -111,68 +148,45 @@ middle_frame.grid(row=1, column=0, sticky=tk.NSEW)
 bottom_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREY)
 bottom_frame.grid(row=2, column=0, sticky=tk.NSEW)
 
-username_label = tk.Label(top_frame, text="Enter username:", font=FONT, bg=DARK_GREY, fg=BLACK)
+username_label = tk.Label(
+    top_frame, text="Enter username:", font=FONT, bg=DARK_GREY, fg=BLACK)
 username_label.pack(side=tk.LEFT, padx=10)
 
-username_textbox = tk.Entry(top_frame, font=FONT, bg=MEDIUM_GREY, fg=BLACK, width=23)
+username_textbox = tk.Entry(
+    top_frame, font=FONT, bg=MEDIUM_GREY, fg=BLACK, width=23)
 username_textbox.bind('<Return>', onIdReturnPress)
 username_textbox.pack(side=tk.LEFT)
 
-username_button = tk.Button(top_frame, text="Join", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=BLACK, command=connect)
+username_button = tk.Button(
+    top_frame, text="Join", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=BLACK, command=connect)
 username_button.pack(side=tk.LEFT, padx=15)
 
-message_textbox = tk.Entry(bottom_frame, font=FONT, bg=MEDIUM_GREY, fg=BLACK, width=28)
+message_textbox = tk.Entry(bottom_frame, font=FONT,
+                           bg=MEDIUM_GREY, fg=BLACK, width=28)
 message_textbox.bind('<Return>', onMessageReturnPress)
 message_textbox.pack(side=tk.LEFT, padx=10)
 
-message_button = tk.Button(bottom_frame, text=emoji.emojize("Send"), font=BUTTON_FONT, bg=OCEAN_BLUE, fg=BLACK, command=send_text)
+message_button = tk.Button(bottom_frame, text=emoji.emojize(
+    "Send"), font=BUTTON_FONT, bg=OCEAN_BLUE, fg=BLACK, command=send_text)
 message_button.pack(side=tk.LEFT, padx=10)
 
 
-send_image_button= tk.Button(bottom_frame, text= "Send Image", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=BLACK, command=send_image)
-send_image_button.pack(side= tk.LEFT)
+send_image_button = tk.Button(bottom_frame, text="Send Image",
+                              font=BUTTON_FONT, bg=OCEAN_BLUE, fg=BLACK, command=send_image)
+send_image_button.pack(side=tk.LEFT)
 
-message_box = scrolledtext.ScrolledText(middle_frame, font=SMALL_FONT, bg=MEDIUM_GREY, fg=BLACK, width=67, height=26.5)
+message_box = scrolledtext.ScrolledText(
+    middle_frame, font=SMALL_FONT, bg=MEDIUM_GREY, fg=BLACK, width=67, height=26.5)
 message_box.config(state=tk.DISABLED)
 message_box.pack(side=tk.TOP)
 
-def handle_img_received(b64image):
-    raw_data = b64decode(b64image)
-    stream = io.BytesIO(raw_data)
-    img = Image.open(stream)
-    
-    global recv_img
-    recv_img = ImageTk.PhotoImage(img)
-    
-    message_box.config(state=tk.NORMAL)
-    message_box.image_create(tk.END, image = recv_img)
-    message_box.config(state=tk.DISABLED)
-    
-    
-
-def listen_for_messages_from_server(client):
-
-    while 1:
-
-        message = client.recv(16384).decode()
-        if message != '':
-            dic_received = eval(message)
-            
-            username = dic_received["sender"]
-            content = dic_received["content"]
-
-            if dic_received["type"] == "image":
-                handle_img_received(content)
-            else:
-                add_message(f"[{username}] {content}")
-            
-        else:
-            messagebox.showerror("Error", "Message recevied from client is empty")
-
 # main function
+
+
 def main():
 
     root.mainloop()
-    
+
+
 if __name__ == '__main__':
     main()
--- server/server.py
+++ server/server.py
@@ -7,7 +7,8 @@ PORT = 1111
 LISTENER_LIMIT = 5
 active_clients = []
 
-def create_message(sender, receiver,message_type, content):
+
+def create_message_dic(sender, receiver, message_type, content):
     return {
         "sender": sender,
         "receiver": receiver,
@@ -15,12 +16,13 @@ def create_message(sender, receiver,message_type, content):
         "content": content
     }
 
+
 def listen_for_messages(client, username):
 
     while 1:
 
         message = client.recv(16394).decode()
-        
+
         if message != '':
             send_messages_to_all(message)
 
@@ -32,28 +34,32 @@ def send_message_to_client(client, message):
 
     client.sendall(message.encode())
 
+
 def send_messages_to_all(message):
-    
+
     for user in active_clients:
 
         send_message_to_client(user[1], message)
 
+
 def client_handler(client):
     while 1:
         message = client.recv(16384).decode()
-        
+
         login_dic = eval(message)
         username = login_dic["sender"]
         if username != '':
             active_clients.append((username, client))
             prompt_message = "SERVER~" + f"{username} added to the chat"
-            dic_to_send = create_message('server', 'all', 'informative', prompt_message)
+            dic_to_send = create_message_dic(
+                'server', 'all', 'informative', prompt_message)
             send_messages_to_all(str(dic_to_send))
             break
         else:
             print("Client username is empty")
 
-    threading.Thread(target=listen_for_messages, args=(client, username, )).start()
+    threading.Thread(target=listen_for_messages,
+                     args=(client, username, )).start()
 
 
 def main():
